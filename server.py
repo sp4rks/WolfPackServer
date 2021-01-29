0b013f96272d19aa156d3e8268d74b7fe2cca21e
Steve <stephen.dillon@outlook.com>
2021-01-29 15:55:28 +1100

    Added CORS

--- server.py
+++ server.py
@@ -1,6 +1,8 @@
 from starlette.applications import Starlette
 from starlette.responses import JSONResponse
 from starlette.routing import Route, Mount
+from starlette.middleware import Middleware
+from starlette.middleware.cors import CORSMiddleware
 
 from v1.app import routes as v1_routes
 
@@ -10,12 +12,20 @@ async def base(request):
         'name': 'Wolfpack Squad Server'
     })
 
+middleware = [
+    Middleware(
+        CORSMiddleware,
+        allow_origins=['*'],
+        allow_methods=['POST']
+    )
+]
+
 routes = [
     Route('/', endpoint=base),
     Mount('/v1', routes=v1_routes)
 ]
 
-app = Starlette(routes=routes)
+app = Starlette(routes=routes, middleware=middleware)
 
 if __name__ == '__main__':
     import uvicorn
